bdbb39e4cd2fde04a297577ae7547ab526b3bb0d
TheDogProjects <maxikajtekinikodem@gmail.com>
2025-05-08 16:28:31 +0200

    Update main.py
    Added POST log, POST warn, POST err And GET cores

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from datetime import datetime
 import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import multiprocessing
 
 
-RULESACCEPT = False
+RULESACCEPT = True
 
 if RULESACCEPT == True:
 
@@ -21,6 +22,39 @@ if RULESACCEPT == True:
             return jsonify({"message": f"Wykonano: {zmienna}"})
         except Exception as e:
             return jsonify({"error": str(e)}), 500
+        
+    @app.route('/localapi/log/<string:txt>', methods=['POST'])
+    def log(txt):
+        now = datetime.now()
+        hour = now.hour
+        minute = now.minute
+        second = now.second
+        print(f"\033[32m[LOG]\033[37m \033[37m[\033[34m{hour}:{minute}:{second}\033[37m] {txt}")
+        return (f"API REQUEST SENT SUCCESSFULLY at \033[37m[\033[34m{hour}:{minute}:{second}\033[37m]")
+    
+
+    @app.route('/localapi/warn/<string:txt>', methods=['POST'])
+    def warn(txt):
+        now = datetime.now()
+        hour = now.hour
+        minute = now.minute
+        second = now.second
+        print(f"\033[33m[WARN]\033[37m \033[37m[\033[34m{hour}:{minute}:{second}\033[37m] {txt}")
+        return (f"API REQUEST SENT SUCCESSFULLY at \033[37m[\033[34m{hour}:{minute}:{second}\033[37m]")
+    
+    @app.route('/localapi/err/<string:txt>', methods=['POST'])
+    def err(txt):
+        now = datetime.now()
+        hour = now.hour
+        minute = now.minute
+        second = now.second
+        print(f"\033[31m[ERROR]\033[37m \033[37m[\033[34m{hour}:{minute}:{second}\033[37m] {txt}")
+        return (f"API REQUEST SENT SUCCESSFULLY at \033[37m[\033[34m{hour}:{minute}:{second}\033[37m]")
+    
+    @app.route('/localapi/info/cpu/cores', methods=['GET'])
+    def get_info():
+        return str(multiprocessing.cpu_count())
+
 
     if __name__ == '__main__':
         app.run(debug=True)
